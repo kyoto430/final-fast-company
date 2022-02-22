23fd42cce053f04661d2d9f8c72328a42e8e6939
kyoto430 <nateriver345@gmail.com>
2022-02-22 10:46:45 +0300

    fix

--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import localStorageService from "./localStorage.service";
 
-const httpAuth = axios.create({
+const REACT_APP_FIREBASE_KEY = "AIzaSyCMa4BQnrF_NkJw11JQGLumSYSRna-vEC4";
+
+export const httpAuth = axios.create({
     baseURL: "https://identitytoolkit.googleapis.com/v1/",
     params: {
-        key: process.env.REACT_APP_FIREBASE_KEY
+        key: REACT_APP_FIREBASE_KEY
     }
 });
 
